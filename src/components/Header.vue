0407c6ad87d73602fa7b6679da8ea8f105b72a2d
coding-jhuunn <codingatjhunn@gmail.com>
2024-08-15 08:55:51 +0800

    done Header

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <div class="left side">Hang Man Game</div>
+    <div class="right side">About</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-around;
+
+  height: 75px;
+}
+
+.side {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: larger;
+  width: 50%;
+}
+
+.side:hover {
+  border-bottom: 1px solid gainsboro;
+}
+</style>
