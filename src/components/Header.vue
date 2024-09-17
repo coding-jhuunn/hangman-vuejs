f62011abd29286f81e0b8bfb15e5928bf7f529fc
coding-jhuunn <codingatjhunn@gmail.com>
2024-09-17 15:28:04 +0800

    add CSS media to Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,6 +16,7 @@ export default {
 </script>
 <style scoped>
 .header {
+  width: 100%;
   display: flex;
   justify-content: space-around;
   height: 75px;
@@ -25,15 +26,39 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: larger;
-  width: 50%;
 }
 
-.side:hover {
-  border-bottom: 1px solid gainsboro;
-}
 a {
   text-decoration: none;
   color: black;
+}
+.right:hover {
+  border-bottom: 1px solid black;
+}
+
+@media (max-width: 600px) {
+  .header {
+    font-size: 14px;
+  }
+}
+@media (max-width: 768px) {
+  .header {
+    font-size: 16px;
+  }
+}
+@media (min-width: 769px) and (max-width: 1024px) {
+  .header {
+    font-size: 18px;
+  }
+}
+@media (min-width: 1025px) and (max-width: 1200px) {
+  .header {
+    font-size: 20px;
+  }
+}
+@media (min-width: 1201px) {
+  .header {
+    font-size: 22px;
+  }
 }
 </style>
